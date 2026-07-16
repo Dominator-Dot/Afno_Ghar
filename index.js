092672d269f7df8a5bb6f86a16db8abf58388afd
Rijen.kayastha <rijen.kayastha.96@gmail.com>
2026-07-16 07:01:41 +0545

    Pushing Database in index

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ app.get('/health', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));const pool = require('./db');
+
+app.get('/db-test', async (req, res) => {
+  const result = await pool.query('SELECT NOW()');
+  res.json(result.rows[0]);
+});
